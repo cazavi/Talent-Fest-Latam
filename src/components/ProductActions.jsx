60917b5e2947d18ca3a1ab502e08763740e63eb3
Carolina <carolinazapatavidal@gmail.com>
2022-10-24 16:44:36 -0300

    Updates

--- src/components/ProductActions.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ProductActions = () => {
-  return (
-    <div className="flex w-[300px] h-[60px] mt-[15px]">
-    <button className="h-[56px] w-[300px] rounded-full pr-10 pl-10 pt-4 pb-4 bg-[#FF6200] text-white grid font-bold text-lg text-center">Agregar al Carro</button>
-    </div>
-  );
-};
--- src/components/ProductImage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ProductImage = ({ product }) => {
-  return (
-    <div className=" flex justify-center items-center">
-      <img src={product.images[0]+'?wid=600&hei=600&qlt=70'} />
-    </div>
-  );
-};
